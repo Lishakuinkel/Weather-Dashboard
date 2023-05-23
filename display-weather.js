ee6032e91e02ef4cccb0303a525e1ba54b5ece6b
Lisha Kuinkel <ltkuinkel@gmail.com>
2023-05-23 09:01:54 -0600

    added local storage

--- display-weather.js
+++ display-weather.js
@@ -3,14 +3,27 @@ var apikey = "5f680b22221a07656a0b13bd681475c7";
 var cityInput = document.getElementById("city-input");
 var searchbtn = document.getElementById("search-btn");
 
+var citysearches = [];
 
-searchbtn.addEventListener('click', displayWeather);
 
-function displayWeather() {
+
+searchbtn.addEventListener('click', getCoordinates);
+
+function getCoordinates() {
     //Fetch geo-coordinates api//   
     let lat;
     let lon;
     
+    
+    var history = JSON.parse(localStorage.getItem("citysearches")) || [];
+    
+    history.push({"citysearches":"cityInput.value"});
+   
+
+    localStorage.setItem("citysearches", JSON.stringify(cityInput.value)); 
+
+    
+
 
     fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput.value + "&limit=1&appid=" + apikey)
         .then(function (response) {
@@ -22,18 +35,25 @@ function displayWeather() {
             console.log(data);
             lat = data[0].lat;
             lon = data[0].lon;
-            return lat, lon;
-        })
+            displayWeather(lat, lon);
+        });
+}
 
-        fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apikey)
+function displayWeather(lat,lon) {
+
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apikey)
         .then(function (response) {
             console.log("success");
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-        })
-    
+            for(var i=0; i<5; i++){
+
+
+            }
+        });
+
 }
 
 
